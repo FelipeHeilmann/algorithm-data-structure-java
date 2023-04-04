4ee32136d150943f83bffd785d6c1d940129c365
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-04-04 16:47:31 -0300

    queue

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        //********************************************************************
         // Stack = LIFO data structure . LIFO -> Last-in First-out
         //      stores objects into a sort of "vertical tower"
         //      push() to add to top
@@ -21,5 +24,33 @@ public class Main {
         //      stack.push("FFVII");
         //
         //      System.out.println(stack);
+        //
+        //********************************************************************
+
+        //********************************************************************
+        // Queure = FIFO data structure. FIFO -> First-in First-out
+        //      A collection designed for holding elements prior processing
+        //      Linear data structure
+        //      is an interface so we cant create an objetc of it, we can a class that implements it
+        //      add = enqueue, offer()
+        //      remove = dequeue, poll()
+        //      size() returns the size of the queue
+        //      isEmpty() returns if the queue is empty
+        //      contains() returns if the queue contains the object
+        //      Queue<String> queue = new LinkedList<>();
+        //
+        //      queue.offer("Ana");
+        //      queue.offer("Victor");
+        //      queue.offer("Felipe");
+        //      queue.offer("Vinicius");
+        //
+        //      queue.poll();
+        //      System.out.println(queue.isEmpty());
+        //      System.out.println(queue.size());
+        //
+        //
+        //      System.out.println(queue);
+        //********************************************************************
+
     }
 }
