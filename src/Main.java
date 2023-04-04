62b17d1f1f0b6bbd9f7280985adc5349859895e6
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-04-04 16:21:43 -0300

    stack

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
+import java.util.Stack;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        
+        // Stack = LIFO data structure . LIFO -> Last-in First-out
+        //      stores objects into a sort of "vertical tower"
+        //      push() to add to top
+        //      pop() to remove from the top
+        //      peek() get the first element in the stack without removing
+        //      search() get the item that you are looking for
+        //      first index is 1
+        //
+        //    Stack<String> stack = new Stack<String>();
+        //
+        //      stack.push("Minecraft");
+        //      stack.push("Skyrim");
+        //      stack.push("DOOM");
+        //      stack.push("Borderlands");
+        //      stack.push("FFVII");
+        //
+        //      System.out.println(stack);
     }
 }
