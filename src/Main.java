b2f9099f12b0b8a3c3e9f840e401a5ee9c7ced2b
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-04-04 20:06:06 -0300

    dynamic array

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         // Stack = LIFO data structure . LIFO -> Last-in First-out
         //      stores objects into a sort of "vertical tower"
@@ -12,18 +12,20 @@ public class Main {
         //      peek() get the first element in the stack without removing
         //      search() get the item that you are looking for
         //      first index is 1
-        //
+
         //    Stack<String> stack = new Stack<String>();
-        //
+
         //      stack.push("Minecraft");
         //      stack.push("Skyrim");
         //      stack.push("DOOM");
         //      stack.push("Borderlands");
         //      stack.push("FFVII");
-        //
+
         //      System.out.println(stack);
-        //
+
         //********************************************************************
+
+
 
         // Queue = FIFO data structure. FIFO -> First-in First-out
         //      A collection designed for holding elements prior processing
@@ -35,25 +37,26 @@ public class Main {
         //      isEmpty() returns if the queue is empty
         //      contains() returns if the queue contains the object
         //      Queue<String> queue = new LinkedList<>();
-        //
+
         //      queue.offer("Ana");
         //      queue.offer("Victor");
         //      queue.offer("Felipe");
         //      queue.offer("Vinicius");
-        //
+
         //      queue.poll();
         //      System.out.println(queue.isEmpty());
         //      System.out.println(queue.size());
-        //
-        //
+
         //      System.out.println(queue);
         //********************************************************************
+
+
 
         // Priority Queue = FIFO data structure that servers elements with the highest priorities first before elements
         //                  with lower priority
         //      Collections.reverseOrder() is an argument passed in the constructor to reverse the sorting of priority queues
         //      Queue<Double> queue = new PriorityQueue<Double>(Collections.reverseOrder());
-        //
+
         //      queue.offer(3.0);
         //      queue.offer(2.5);
         //      queue.offer(4.0);
@@ -64,6 +67,8 @@ public class Main {
         //      System.out.println(queue.poll());
         //      }
         //********************************************************************
+
+
 
         // LinkedList = stores Nodes in two parts (data + address)
         //      Nodes are non-consecutive memory locations
@@ -79,6 +84,8 @@ public class Main {
         //      You can work with a linkedlist either as a queue or a stack, by using their own methods
         //         LinkedList<String> linkedList = new LinkedList<>();
         //
+
+        //       Stack:
         //       linkedList.push("A");
         //       linkedList.push("B");
         //       linkedList.push("C");
@@ -88,6 +95,7 @@ public class Main {
         //
         //       System.out.println(linkedList);
 
+        //      Queue
         //       linkedList.offer("A");
         //       linkedList.offer("B");
         //       linkedList.offer("C");
@@ -99,9 +107,29 @@ public class Main {
         //       System.out.println(linkedList.indexOf("F"));
         //       System.out.println(linkedList.peekFirst());
         //       System.out.println(linkedList.peekLast());
+
         //       linkedList.addFirst("0");
         //       linkedList.addLast("G");
         //       System.out.println(linkedList);
+        //********************************************************************
+
+        //       DynamicArray dynamicArray = new DynamicArray(5);
+        //        dynamicArray.add("A");
+        //        dynamicArray.add("B");
+        //        dynamicArray.add("C");
+        //        dynamicArray.add("D");
+        //        dynamicArray.add("E");
+        //
+        //        dynamicArray.delete("A");
+        //        dynamicArray.delete("B");
+        //        dynamicArray.delete("C");
+        //        dynamicArray.delete("D");
+        //        dynamicArray.insert(0, "X");
+        //        dynamicArray.delete("X");
+        //        System.out.println(dynamicArray.search("C"));
+
+        //      System.out.println(dynamicArray);
+        //       System.out.println(dynamicArray.isEmpty());
 
     }
 }
