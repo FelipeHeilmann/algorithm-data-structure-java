5efe928fdc855c23ead080ad262335f4fa59d2d7
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-04-05 15:34:27 -0300

    interpolation search

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -24,22 +24,44 @@ public class Algorithm {
             //System.out.println("Element not found");
         //}
 
+
+
         // binary search = Search algorithm that finds the position of a target value within a sorted array.
         //                  Half of the array is eliminated during each "step"
 
-        int array[] = new int[1000000];
-        int target = 777777;
-        for(int i =0; i < array.length; i++){
-            array[i] = i;
-        }
-        int index = binarySearch(array, target);
+        //int[] array = new int[1000000];
+        //int target = 777777;
+        //for(int i =0; i < array.length; i++){
+        //    array[i] = i;
+        //}
+        //int index = binarySearch(array, target);
+        //if(index != -1){
+        //    System.out.println("Element found at index : " + index);
+        //}
+        //else{
+        //    System.out.println("Element not found");
+        //}
+
+
+
+        // interpolation search = improvement over binary search best used for "uniformly" distributed data "guesses"
+        //                        where a value might be based on calculated probes results
+        //                        if probe is incorrect, search area is narrowed, and a new probe is calculated
+
+        //                        average case: O(log(log(n)))
+        //                        worst case: O(n) [values increase exponentially]
+
+        int[] array = {1,2,4,6,8,10,12,14,16,18,20};
+        int index =  interpolationSearch(array, 8);
         if(index != -1){
-            System.out.println("Element found at index : " + index);
+            System.out.println("Element found at index: " + index);
         }
         else{
             System.out.println("Element not found");
         }
+
     }
+
 
     private static int linearSearch(int[] arr, int value){
         for(int i = 0; i < arr.length; i++ ){
@@ -69,5 +91,27 @@ public class Algorithm {
            }
        }
        return 0;
+    }
+
+    private static int interpolationSearch(int[] array, int value) {
+        int high = array.length-1;
+        int low = 0;
+
+        //search area
+        while(value >= array[low] && value <= array[high] && low<= high){
+            int probe = low + (high-low) * (value-array[low]) / (array[high] - array[low]);
+            System.out.println("probe: " + probe);
+
+            if(array[probe] == value){
+                return probe;
+            }
+            else if(array[probe] < value){
+                low = probe + 1;
+            }
+            else{
+                high = probe -1;
+            }
+        }
+        return -1;
     }
 }
