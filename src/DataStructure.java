61b8d57cc417b9b4db751f78b932fb0fcb10fdf9
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-04-05 14:46:30 -0300

    linear search

--- src/DataStructure.java
+++ src/DataStructure.java
@@ -1,8 +1,6 @@
-import java.util.*;
-
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class DataStructure {
     public static void main(String[] args){
 
         // Stack = LIFO data structure . LIFO -> Last-in First-out
