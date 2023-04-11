12f8d8fd29c47111de32160925476a971e5c3a74
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-04-11 15:48:04 -0300

    insertion sort

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -82,11 +82,25 @@ public class Algorithm {
         //                  small data set = okay
         //                  large data set = BAD
 
-            int[] array = {8, 7, 9, 2, 3, 1, 5, 4, 6};
-            selectionSort(array);
-            for(int i : array){
-                System.out.print(i);
-            }
+        //    int[] array = {8, 7, 9, 2, 3, 1, 5, 4, 6};
+        //    selectionSort(array);
+        //    for(int i : array){
+        //        System.out.print(i);
+        //    }
+
+        // insertion sort = after comparing elements to the left, shift elements to the right to make room to insert a
+        //                  value
+        //                  Quadratic time O(n²)
+        //                  small data set = decent
+        //                  large data set = BAD
+        //                  less steps than bubble sort
+        //                  best case is O(n) compared to selection sort O(n²)
+
+        int[] array = {9,1,8,2,7,3,6,5,4};
+        insertionSort(array);
+        for(int i : array){
+            System.out.print(i + " ");
+        }
     }
 
 
@@ -164,4 +178,18 @@ public class Algorithm {
             array[min] = temp;
         }
     }
+    private static void insertionSort(int[] array) {
+        for(int i = 1; i < array.length; i++){
+            int temp = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > temp){
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = temp;
+
+        }
+    }
+
 }
