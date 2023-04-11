08152bb37534f2cf23f750febcd2d23ef0c5c7d9
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-04-11 16:37:51 -0300

    merge sort

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -96,10 +96,21 @@ public class Algorithm {
         //                  less steps than bubble sort
         //                  best case is O(n) compared to selection sort O(n²)
 
-        int[] array = {9,1,8,2,7,3,6,5,4};
-        insertionSort(array);
+        //  int[] array = {9,1,8,2,7,3,6,5,4};
+        //  insertionSort(array);
+        //  for(int i : array){
+        //    System.out.print(i + " ");
+        //}
+
+        // merge sort = recursively divide array in 2, sort, re-combine
+        //
+        //              run time complexity: Quasilinear time O(n log n)
+        //              space complexity: linear O(n)
+
+        int[] array={8,2,5,3,4,7,6,1};
+        mergeSort(array);
         for(int i : array){
-            System.out.print(i + " ");
+            System.out.println(i + " ");
         }
     }
 
@@ -189,6 +200,62 @@ public class Algorithm {
             }
             array[j+1] = temp;
 
+        }
+    }
+
+    private static void mergeSort(int[] array){
+        int length = array.length;
+        if(length <= 1){
+            return; // base case
+        }
+        int middle = length/2;
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[length-middle];
+
+        int i = 0; // left array
+        int j = 0; // right array
+
+        for(;i < length; i ++ ){
+            if(i < middle){
+                leftArray[i] = array[i];
+            }
+            else{
+                rightArray[j] = array[i];
+                j++;
+            }
+        }
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(leftArray, rightArray, array);
+    }
+
+    private static void merge(int[] leftArray, int[] rightArray, int[] array) {
+        int leftSize = array.length / 2;
+        int rightSize = array.length - leftSize;
+
+        int i = 0, left = 0, right = 0;
+
+        while (left < leftSize && right < rightSize){
+            if(leftArray[left] < rightArray[right]){
+                array[i] = leftArray[left];
+                i++;
+                left++;
+            }
+            else{
+                array[i] = rightArray[right];
+                i++;
+                right++;
+            }
+        }
+        while(left < leftSize){
+            array[i] = leftArray[left];
+            i++;
+            left++;
+        }
+        while (rightSize < rightSize){
+            array[i] = rightArray[right];
+            i++;
+            right++;
         }
     }
 
