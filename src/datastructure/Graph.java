bce49b55d742d2ad4af85f0bfa8ea3af878c12db
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-04-17 17:32:31 -0300

    grap as adjacency list

--- src/datastructure/Graph.java
+++ src/datastructure/Graph.java
@@ -1,39 +1,65 @@
 package datastructure;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Graph {
     ArrayList<Node> nodes;
+    ArrayList<LinkedList<Node>> alist;
     int[][] matrix;
 
-    Graph(int size){
-        nodes = new ArrayList<>();
-        matrix = new int[size][size];
+    Graph(){
+        alist = new ArrayList<>();
     }
+    //Graph(int size){
+        //nodes = new ArrayList<>();
+        //matrix = new int[size][size];
+    //}
 
     public void addNode(Node node){
-        nodes.add(node);
+        LinkedList<Node> currentList = new LinkedList<>();
+        currentList.add(node);
+        alist.add(currentList);
+        //nodes.add(node);
     }
 
     public void addEdge(int source, int destination){
-        matrix[source][destination] = 1;
+        LinkedList<Node> currentList = alist.get(source);
+        Node destiNode = alist.get(destination).get(0);
+        currentList.add(destiNode); //adding the node at the tail of the linkedlist
+
+        //matrix[source][destination] = 1;
     }
 
     public boolean checkEdge(int source, int destination){
-        return matrix[source][destination] == 1;
+        LinkedList<Node> currentList = alist.get(source);
+        Node destiNode = alist.get(destination).get(0);
+        for(Node node: currentList){
+            if(node== destiNode) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void print(){
-        System.out.print("  ");
-        for(Node node : nodes){
-            System.out.print(node.data + " ");
-        }
-        System.out.println();
+        //System.out.print("  ");
+        //for(Node node : nodes){
+            //System.out.print(node.data + " ");
+        //}
+        //System.out.println();
 
-        for(int i = 0; i < matrix.length; i++){
-            System.out.print(nodes.get(i).data + " " );
-            for(int j = 0;  j < matrix[i].length; j++){
-                System.out.print(matrix[i][j] + " ");
+        //for(int i = 0; i < matrix.length; i++){
+            //System.out.print(nodes.get(i).data + " " );
+            //for(int j = 0;  j < matrix[i].length; j++){
+                //System.out.print(matrix[i][j] + " ");
+            //}
+            //System.out.println();
+        //}
+
+        for (LinkedList<Node> currentList : alist){
+            for(Node node : currentList){
+                System.out.print(node.data + "->");
             }
             System.out.println();
         }
--- src/datastructure/GraphTest.java
+++ src/datastructure/GraphTest.java
@@ -9,23 +9,50 @@ public class GraphTest {
         //                    runtime complexity = O(1)
         //                    space complexity = O(n²)
 
-        Graph graph = new Graph(5);
+       // Graph graph = new Graph(5);
 
-        graph.addNode(new Node('A'));
-        graph.addNode(new Node('B'));
-        graph.addNode(new Node('C'));
-        graph.addNode(new Node('D'));
-        graph.addNode(new Node('E'));
+        //graph.addNode(new Node('A'));
+        //graph.addNode(new Node('B'));
+        //graph.addNode(new Node('C'));
+        //graph.addNode(new Node('D'));
+        //graph.addNode(new Node('E'));
 
-        graph.addEdge(0,1);
-        graph.addEdge(1,2);
-        graph.addEdge(2,3);
-        graph.addEdge(2,4);
-        graph.addEdge(4,0);
-        graph.addEdge(4,2);
+        //graph.addEdge(0,1);
+        //graph.addEdge(1,2);
+        //graph.addEdge(2,3);
+        //graph.addEdge(2,4);
+        //graph.addEdge(4,0);
+        //graph.addEdge(4,2);
 
-        graph.print();
+        //graph.print();
 
-        System.out.println(graph.checkEdge(2,1));
+        //System.out.println(graph.checkEdge(2,1));
+
+        //adjacency list = An array/arraylist of linked liskedlist.
+        //                  Each LikedList has a unique node at the head.
+        //                  All adjacent neighbors to that node are added to that node's linkedList
+
+        //                  runtime complexity: O(n)
+        //                  space complexity: O(n + e)
+
+        int[] array = new int[5];
+        Graph graph1 = new Graph();
+        graph1.addNode(new Node('A'));
+        graph1.addNode(new Node('B'));
+        graph1.addNode(new Node('C'));
+        graph1.addNode(new Node('D'));
+        graph1.addNode(new Node('D'));
+
+        graph1.addEdge(0,1);
+        graph1.addEdge(1,2);
+        graph1.addEdge(1,4);
+        graph1.addEdge(2,3);
+        graph1.addEdge(2,4);
+        graph1.addEdge(4,0);
+        graph1.addEdge(4,2);
+
+        graph1.print();
+
+
     }
 }
